c31406212025156ecbff120c366696619f6d6860
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-18 22:25:43 +0530

    Git commands

--- .commands.sh
+++ .commands.sh
@@ -9,5 +9,12 @@ function create-repo(){
      cd /path/to/your/projects/folder/
      mkdir $1
      cd $1
-     
+     git init
+     git remote add origin  https://github.com/$username/$1.git
+     touch README.MD
+     git add .
+     git commit -m "Initial Commit"
+     git push -u origin master
+     code .
+     echo Project created and pushed to Github 
 }
