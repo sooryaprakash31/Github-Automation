f6ada6f8bf1dcb46d5b6f0f67864bc802499a762
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-18 22:22:18 +0530

    Creating project folder commands

--- .commands.sh
+++ .commands.sh
@@ -5,4 +5,9 @@ function create-repo(){
      read -p "Github Username: " username
      read -s -p "Github Password: " password
      python3 github_automate.py $1 username password
+     cd
+     cd /path/to/your/projects/folder/
+     mkdir $1
+     cd $1
+     
 }
