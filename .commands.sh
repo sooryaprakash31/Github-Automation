02bc0b3c6458446759adabe509b464389183b37c
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-08-24 00:00:17 +0530

    Updated readme

--- .commands.sh
+++ .commands.sh
@@ -3,7 +3,7 @@
 function createrepo(){
      read -p "Github Username: " username
      read -s -p "Github Password: " password
-     python3 github_automate.py $1 $username $password
+     python3 /usr/bin/github_automate.py $1 $username $password
      cd
      cd /path/to/your/projects/folder/
      mkdir $1
